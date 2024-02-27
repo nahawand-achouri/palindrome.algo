3c0e359ef83e8b74f59597cc4d72e42d54caa6f1
nahawand <nahaachouri@gmail.com>
2024-02-27 14:56:08 +0100

    dom.checkpoint

---
+++ js/exercice.js
@@ -0,0 +1,13 @@
+function test (a,b) {
+    if (a === 100 || b=== 100 || a+b=== 100){
+        return true
+
+    }
+    else{
+        return false
+    }
+    
+
+}
+  console.log(test(100,20))
+  
